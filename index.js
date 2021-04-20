97f2bc6929d1403100cf8d4433b37f26d7ebc5e4
userlab-cesarsalazar <cesar.augs@mgail.com>
2021-04-20 13:18:08 -0500

    add request lib

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { rejects } = require("assert");
 const express = require("express");
+const { resolve } = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const request = require("request");
   const fs = require("fs");
   console.log(__dirname, "__dirname");
@@ -20,10 +22,16 @@ app.get("/", (req, res) => {
     },
   };
   console.log(options, "options");
-  request.get(options, (error, response, body) => {
-    console.log(error, "error");
-    console.log(response, "response");
-    console.log(body, "body");
+  await new Promise((resolve, rejects) => {
+    request.get(options, (error, response, body) => {
+      if (error) {
+        rejects(error);
+        console.log(error, "error");
+      }
+      console.log(response, "response");
+      console.log(body, "body");
+      resolve(response);
+    });
   });
   console.log("end");
   res.send("Hello World!");
