c35c321a75de78aa6078a2d60062bbcbe881b53e
userlab-cesarsalazar <cesar.augs@mgail.com>
2021-04-20 10:55:38 -0500

    add request lib

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  const request = require("request");
+  const fs = require("fs");
+
+  var options = {
+    url:
+      "https://api.blackhawknetwork.com/productCatalogManagement/v1/productCatalogs",
+    headers: {
+      "content-type": "application/json",
+    },
+    agentOptions: {
+      pfx: fs.readFileSync(__dirname + "/certs/myCert.p12"),
+      passphrase: "",
+    },
+  };
+
+  request.get(options, (error, response, body) => {
+    console.log(error);
+    console.log(response);
+    console.log(body);
+  });
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
