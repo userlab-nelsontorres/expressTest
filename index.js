79f6c4af6edfcb6045074b671c84db01dbc92798
userlab-cesarsalazar <cesar.augs@mgail.com>
2021-04-22 13:49:35 -0500

    fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+/*const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const app = express();
 const port = 3000;
-/*
+
 app.get("/", (req, res) => {
   const headers = {
     "content-type": "application/json",
